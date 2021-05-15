312b339afa05dd5405fa1832268dc8b17d6002a9
Hennie <2749126@naver.com>
2021-05-15 20:03:45 +0900

    Memo - Controller (+ 메모 수정, 삭제)
    - MemoController (메모 수정, 개별 삭제, 선택 삭제)

--- src/main/java/memo/MemoController.java
+++ src/main/java/memo/MemoController.java
@@ -53,6 +53,40 @@ public class MemoController extends HttpServlet	{
 			String page = "/memo/memo_view.jsp";
 			RequestDispatcher rd = request.getRequestDispatcher(page);
 			rd.forward(request, response);
+		
+		// delete one
+		} else if(url.indexOf("del.do") != -1) {
+			int idx = Integer.parseInt(request.getParameter("idx"));	// 글번호
+			dao.deleteMemo(idx);
+			// 메모리스트로 이동
+			response.sendRedirect(request.getContextPath() + "/memo/memo.jsp");
+
+		// delete selected
+		} else if(url.indexOf("delete_all.do") != -1) {
+			// 파라미터 배열
+			String[] idx = request.getParameterValues("idx");
+			if(idx != null) {
+				for(int i=0; i<idx.length; i++) {
+					// 배열을 순회하면서 삭제 처리
+					dao.deleteMemo(Integer.parseInt(idx[i]));
+				}
+			}
+			// 메모리스트로 이동
+			response.sendRedirect(request.getContextPath() + "/memo/memo.jsp");
+		
+		// update
+		} else if(url.indexOf("update.do") != -1) {
+			int idx = Integer.parseInt(request.getParameter("idx"));
+			String writer = request.getParameter("writer");
+			String memo = request.getParameter("memo");
+			MemoDTO dto = new MemoDTO();
+			dto.setIdx(idx);
+			dto.setWriter(writer);
+			dto.setMemo(memo);
+			// 레코드 수정
+			dao.updateMemo(dto);
+			// 메모리스트로 이동
+			response.sendRedirect(request.getContextPath() + "/memo/memo.jsp");
 		}
 	}
 	
