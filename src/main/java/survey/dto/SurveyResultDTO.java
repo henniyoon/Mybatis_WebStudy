8231744bbe369c34e3fcb98da7eb33d65d98486d
Hennie <2749126@naver.com>
2021-05-17 23:50:29 +0900

    Survey - Model (DTO)
    - SurveyDTO (설문 문항)
    - SurveyResultDTO (설문 응답)
    - SurveySummaryDTO (응답 결과 요약)

---
+++ src/main/java/survey/dto/SurveyResultDTO.java
@@ -0,0 +1,28 @@
+package survey.dto;
+
+public class SurveyResultDTO {
+	private int answer_idx;
+	private int survey_idx;
+	private int num;
+	
+	// Getter, Setter
+	public int getAnswer_idx() {
+		return answer_idx;
+	}
+	public void setAnswer_idx(int answer_idx) {
+		this.answer_idx = answer_idx;
+	}
+	public int getSurvey_idx() {
+		return survey_idx;
+	}
+	public void setSurvey_idx(int survey_idx) {
+		this.survey_idx = survey_idx;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
