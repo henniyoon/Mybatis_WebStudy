ff7fa8da65e395f4dde5721a3bfebc4970d26fce
Hennie <2749126@naver.com>
2021-05-24 15:45:00 +0900

    Page - Model
    - EmpDTO

---
+++ src/main/java/page/EmpDTO.java
@@ -0,0 +1,22 @@
+package page;
+
+public class EmpDTO {
+	
+	private int empno;
+	private String ename;
+	
+	// Getter, Setter
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+}
