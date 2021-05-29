059601e9d6c4195af54a9ee10643d25262a9ce5a
Hennie <2749126@naver.com>
2021-05-29 17:18:06 +0900

    common

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,8 @@
+package common;
+
+public class Constants {
+	// 파일 업로드 디렉토리
+	public static final String UPLOAD_PATH = "c:/upload/";
+	// 파일 업로드 최대 용량
+	public static final int MAX_UPLOAD = 10 * 1024 * 1024;
+}
