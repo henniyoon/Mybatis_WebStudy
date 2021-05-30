df71fe8ad6e7ebd889c7c481747abf458e4e50d2
Hennie <2749126@naver.com>
2021-05-30 22:46:28 +0900

    Board - Model
    - BoaradDAO (리스트)

---
+++ src/main/java/board/dao/BoardDAO.java
@@ -0,0 +1,80 @@
+package board.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import board.dto.BoardDTO;
+import sqlmap.MybatisManager;
+
+public class BoardDAO {
+	
+	private static BoardDAO instance;	// 싱글톤 인스턴스
+	
+	private BoardDAO() {}	// 외부에서 호출할 수 없는 생성자
+	
+	public static BoardDAO getInstance() {
+		if(instance == null) {
+			instance = new BoardDAO();	// 인스턴스 생성
+		}
+		return instance;
+	}
+	
+	public int count(String search_option, String keyword) {
+		int result = 0;
+		
+		try(SqlSession session = MybatisManager.getInstance().openSession()) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("search_option", search_option);
+			map.put("keyword", keyword);
+			result = session.selectOne("board.search_count", map);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public List<BoardDTO> searchList(String searchOption, String keyword, 
+			int start, int end) {
+		List<BoardDTO> list = null;
+		
+		try(SqlSession session = MybatisManager.getInstance().openSession()) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("search_option", searchOption);
+			map.put("keyword", keyword);
+			map.put("start", start);
+			map.put("end", end);
+			System.out.println(map);
+			list = session.selectList("board.searchList", map);
+			for(BoardDTO dto : list) {
+				String writer = dto.getWriter();
+				String subject = dto.getSubject();
+				switch(searchOption) {
+					case "all":
+						writer = writer.replace(keyword,
+								"<span style='color:red'>" + keyword + "</span>");
+						subject = subject.replace(keyword,
+								"<span style='color:red'>" + keyword + "</span>");
+						break;
+					case "writer":
+						writer = writer.replace(keyword, 
+								"<span style='color:red'>" + keyword + "</span>");
+						break;
+					case "subject":
+						subject = subject.replace(keyword, 
+								"<span style='color:red'>" + keyword + "</span>");
+						break;
+				}
+				dto.setWriter(writer);
+				dto.setSubject(subject);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+
+}
